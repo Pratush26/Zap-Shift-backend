0a570adefc32693f9c04b37a7d4bcfac4cc04add
Pratush <pratushbarua26@gmail.com>
2025-11-16 23:50:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 2000;
+const uri = process.env.DB;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Server is getting!"))
+
+app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
